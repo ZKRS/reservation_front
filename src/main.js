ae226f9c8cd5490e342ba819850374b7271a4430
ZKRS <2585122837@qq.com>
2018-04-23 18:44:58 +0800

    2018-04-23

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  el: '#app', // 挂载点是app, 即index.html中的<div id="app"> </div>上
+  router, // 使用的路由规则, 定义在./router/index.js文件中
+  components: { App }, // 内部组成部分有App.vue, 即当前目录下的App.vue文件, import App from './App'
+  template: '<App/>' //
 })
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+Vue.use(Router) // Vue中使用vue-router这个插件
 
 export default new Router({
   routes: [
